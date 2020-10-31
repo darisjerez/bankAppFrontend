db7cf30c91bb98a2ca35b98a6453a52253536719
darisjerez <darisjerez@gmail.com>
2020-10-30 21:12:55 -0400

    add bootstrap, add benefits, header and carousel components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+
+import { BenefitsComponent } from './components/benefits/benefits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarouselComponent,
+    BenefitsComponent
   ],
   imports: [
     BrowserModule,
